be4fe39b9763135ecb6915bd21dd95fb67ac5fef
Tarun Reddy <tarunreddy.04@gmail.com>
2020-04-23 22:16:55 -0400

    Add Intro component. Update navbar, footer.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
-    $('.navbar-wrapper').load('/app/navbar/navbar.html');
-    $('.footer-wrapper').load('/app/footer/footer.html');
+  $('.navbar-wrapper').load('/app/navbar/navbar.html');
+  $('.intro-wrapper').load('/app/intro/intro.html');
+  $('.experience-wrapper').load('/app/experience/experience.html');
+  $('.footer-wrapper').load('/app/footer/footer.html');
 });
