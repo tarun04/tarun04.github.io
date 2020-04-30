0831b7f445b8b9fd89a8259c2cee4ba4309119fa
tarunreddy04 <tarunreddy.04@gmail.com>
2020-04-29 22:35:37 -0400

    Add home, about, projects components. Update navbar UI.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 $(document).ready(() => {
-  $('.navbar-wrapper').load('/app/navbar/navbar.html');
-  $('.intro-wrapper').load('/app/intro/intro.html');
+  $('.home-wrapper').load('/app/home/home.html');
+  $('.about-wrapper').load('/app/about/about.html');
   $('.experience-wrapper').load('/app/experience/experience.html');
+  $('.projects-wrapper').load('/app/projects/projects.html');
+  $('.contact-wrapper').load('/app/contact/contact.html');
   $('.footer-wrapper').load('/app/footer/footer.html');
 });
---
+++ scripts/navbar.js
@@ -0,0 +1,105 @@
+setTimeout(function() {
+  var isMobile;
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    isMobile = true;
+
+    // Mobile height fix
+    $('.height-fix').each(function() {
+      var h = $(this).height();
+      $(this).height(h);
+    });
+  }
+
+  // Sticky Nav on Mobile
+  if (isMobile) {
+    $('nav').addClass('fixed');
+  } else {
+    $('nav').addClass('desk');
+  }
+
+
+  // NAV POSITION
+  var navPos = $('nav').position().top;
+  var lastPos = 0;
+  var lockTimer;
+  $(window).on('scroll', function() {
+    var pos = $(window).scrollTop();
+    var pos2 = pos + 50;
+    var scrollBottom = pos + $(window).height();
+
+    if (!isMobile) {
+      if (pos >= navPos + $('nav').height() && lastPos < pos) {
+        $('nav').addClass('fixed');
+      }
+      if (pos < navPos && lastPos > pos) {
+        $('nav').removeClass('fixed');
+      }
+      lastPos = pos;
+    }
+    
+    //Link Highlighting
+    if (pos2 > $('#home').offset().top) {
+      highlightLink('home');
+      $('nav').css('border-color', '#fff')
+    }
+    if (pos2 > $('#about').offset().top) {
+      highlightLink('about');
+      $('nav').css('border-color', '#4285f4')
+    }
+    if (pos2 > $('#experience').offset().top) {
+      highlightLink('experience');
+      $('nav').css('border-color', '#ea4336')
+    }
+    if (pos2 > $('#projects').offset().top) {
+      highlightLink('projects');
+      $('nav').css('border-color', '#fbbc05')
+    }
+    if (
+      pos2 > $('#contact').offset().top ||
+      pos + $(window).height() === $(document).height()
+    ) {
+      highlightLink('contact');
+      $('nav').css('border-color', '#34a853')
+    }
+
+    // Prevent Hover on Scroll
+    clearTimeout(lockTimer);
+    if (!$('body').hasClass('disable-hover')) {
+      $('body').addClass('disable-hover');
+    }
+
+    lockTimer = setTimeout(function() {
+      $('body').removeClass('disable-hover');
+    }, 500);
+  });
+  
+  function highlightLink(anchor) {
+    $('nav .active').removeClass('active');
+    $('nav')
+      .find('[dest="' + anchor + '"]')
+      .addClass('active');
+  }
+
+  // EVENT HANDLERS
+  $('.nav-link').click(function() {
+    var x = $('.nav-link');
+    var anchor = $(this).attr('dest');
+    $('.navbar-nav').removeClass('visible');
+
+    $('nav span').removeClass('active');
+    $('nav')
+      .find('[dest="' + anchor + '"]')
+      .addClass('active');
+
+    $('html, body').animate(
+      {
+        scrollTop: $('#' + anchor).offset().top
+      },
+      400
+    );
+  });
+
+  $('.fa-bars').click(function() {
+    $('.navbar-nav').toggleClass('visible');
+  });
+}, 3000);
